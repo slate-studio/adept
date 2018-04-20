ab34b5a513d01c60a57b1a4fdb0ac9fd39504c4b
Alexander Kravets <alex@slatestudio.com>
2018-04-20 19:27:10 +0300

    Add validation logic to definition

--- test/index.js
+++ test/index.js
@@ -1,16 +1,40 @@
 'use strict'
 
+const units = require('./units')
+const { expect } = require('chai')
+
 describe('lib', () => {
+
   describe('compose', () => {
+
     it('should build specification for modules', () => {
-      const units = require('./units')
       const { Composer } = require('../lib')
 
       const modules  = [ units ]
       const composer = new Composer({ modules })
-      const yaml = composer.yaml
+      const yaml = composer.yaml // eslint-disable-line no-unused-vars
 
-      console.log(yaml) // eslint-disable-line no-console
+      // console.log(yaml) // eslint-disable-line no-console
     })
+
   })
+
+  describe('validator', () => {
+
+    it('should validate object against schema', async() => {
+      await units.UpdateUnitInput.validate({ name: 'Unit #1' })
+    })
+
+    it('should raise validation exception with errors', async() => {
+      try {
+        await units.Unit.validate({ name: 'Unit #1' })
+
+      } catch (errors) {
+        expect(errors.length).to.equal(17)
+
+      }
+    })
+
+  })
+
 })
