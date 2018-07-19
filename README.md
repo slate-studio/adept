e60b14b27997f5321fec6a21c54579e30c0fae19
Alexander Kravets <a@kra.vc>
2018-07-19 12:13:57 +0300

    Remove travis integration as it fails to run tests

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-[![Build Status](https://travis-ci.org/slate-studio/adept.svg?branch=master)](https://travis-ci.org/slate-studio/adept)
 [![Known Vulnerabilities](https://snyk.io/test/github/slate-studio/adept/badge.svg?targetFile=package.json)](https://snyk.io/test/github/slate-studio/adept?targetFile=package.json)
 
 # Adept
--- test/schema.spec.js
+++ test/schema.spec.js
@@ -2,18 +2,18 @@
 
 const Schema     = require('../lib/schema')
 const { expect } = require('chai')
-// const UpdateUnitInput = require('../examples/api/units/updateUnitInput')
-// const CreateUnitInput = require('../examples/api/units/createUnitInput')
+const UpdateUnitInput = require('../examples/api/units/updateUnitInput')
+const CreateUnitInput = require('../examples/api/units/createUnitInput')
 const { JobState }    = require('../examples/schemas')
 
 describe('Schema', () => {
 
-  // describe('Schema.load()', () => {
-  //   it('should raise exception if schema validation fails', async() => {
-  //     const load = () => Schema.load('examples/schemas', { validatorOptions: { strictMode: true }})
-  //     expect(load).to.throw()
-  //   })
-  // })
+  describe('Schema.load()', () => {
+    it('should raise exception if schema validation fails', async() => {
+      const load = () => Schema.load('examples/schemas', { validatorOptions: { strictMode: true }})
+      expect(load).to.throw()
+    })
+  })
 
   describe('.schemas', () => {
     it('should return empty hash if was not set', async() => {
@@ -22,23 +22,23 @@ describe('Schema', () => {
     })
   })
 
-  // describe('.validate()', () => {
-  //   it('should validate object', async() => {
-  //     await UpdateUnitInput.schema.validate({ name: 'Unit #1' })
-  //   })
+  describe('.validate()', () => {
+    it('should validate object', async() => {
+      await UpdateUnitInput.schema.validate({ name: 'Unit #1' })
+    })
 
-  //   it('should raise exception if object is not valid', async() => {
-  //     try {
-  //       await CreateUnitInput.schema.validate({ name: 'Unit #1' })
+    it('should raise exception if object is not valid', async() => {
+      try {
+        await CreateUnitInput.schema.validate({ name: 'Unit #1' })
 
-  //     } catch (errors) {
-  //       expect(errors.length).to.equal(5)
-  //       return
-  //     }
+      } catch (errors) {
+        expect(errors.length).to.equal(5)
+        return
+      }
 
-  //     throw new Error('Expected exception has not been raised')
-  //   })
-  // })
+      throw new Error('Expected exception has not been raised')
+    })
+  })
 
   describe('.updateDefaults()', () => {
     it('should ignore parent schemas of non-object type', async() => {
