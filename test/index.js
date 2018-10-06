4ffb0b4c1251c6e9447c9e9981d9088f6cac45a8
Alexander Kravets <a@kra.vc>
2018-10-06 09:21:46 +0800

    Drop Document class

--- test/index.js
+++ test/index.js
@@ -3,6 +3,5 @@
 require('./lib/Schema.spec')
 require('./lib/Component.spec')
 require('./lib/Operation.spec')
-require('./lib/Document.spec')
 require('./lib/Security.spec')
 // require('./lib/Compose.spec')
